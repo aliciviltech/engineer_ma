6e4c95a9afcf60d20fac013a55fcddb5b13b0d02
aliciviltech <ali.civiltech@gmail.com>
2025-02-14 19:26:59 +0500

    weather forecast adjusted

--- src/app/components/Weather/Weather.tsx
+++ src/app/components/Weather/Weather.tsx
@@ -49,12 +49,14 @@ interface WeatherDataType {
 
 
 const Weather = () => {
-    const myAPI_key = '13c34acc87a5f0efc6e2bf5854ae8a71';
+    console.log(process.env.NEXT_PUBLIC_WEATHER_API_KEY)
+    const myAPI_key = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
     const [weatherData, setWeatherData] = useState<WeatherDataType[]>([]);
     const [timeNow, setTimeNow] = useState<string>();
     const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     const todayNumber = new Date().getDay()
     const today = dayNames[todayNumber]
+    const otherDays = dayNames.slice(todayNumber + 1, 7).concat(dayNames.slice(0, todayNumber))
     useEffect(() => {
         const interval = setInterval(() => {
             const extractTimeNow = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
@@ -74,7 +76,7 @@ const Weather = () => {
                 return data
             })
     }
-    
+
     useEffect(() => {
         const cities = ['karachi', 'lahore', 'islamabad', 'queta', 'peshawar'];
         Promise.all(cities.map(fetchWeather))
@@ -94,54 +96,57 @@ const Weather = () => {
             {/* left side */}
             {
                 weatherData.length > 0
-                ?
-                <div className="leftSide relative text-white w-full xl:w-1/2 flex flex-col justify-evenly sm:justify-between gap-10">
-                    <div className="icon w-full sm:h-full sm:absolute text-[60px] text-center flex justify-center items-center">
-                        <div className='flex flex-col items-center justify-center '>
-                            <Image className='w-32 h-32' src={`/images/weather/${weatherData[0]?.weather[0].icon}.png`} alt='icon' width={300} height={300} />
-                            <p className='text-[22px]'>{weatherData[0]?.weather[0].main}</p>
+                    ?
+                    <div className="leftSide relative text-white w-full xl:w-1/2 flex flex-col justify-evenly sm:justify-between gap-10">
+                        <div className="icon w-full sm:h-full sm:absolute text-[60px] text-center flex justify-center items-center">
+                            <div className='flex flex-col items-center justify-center '>
+                                <Image className='w-32 h-32' src={`/images/weather/${weatherData[0]?.weather[0].icon}.png`} alt='icon' width={300} height={300} />
+                                <p className='text-[22px]'>{weatherData[0]?.weather[0].main}</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="today flex justify-between sm:items-center flex-col sm:flex-row gap-4">
-                        <div className="temperatureSide">
-                            <h1 className='text-[clamp(20px,20vw,60px)] sm:text-[60px]'>{Math.round(weatherData[0]?.main.temp)}<sup className=''>o</sup>C</h1>
-                            <p className='text-[clamp(16px,5vw,22px)] sm:text-sm'>Precepitation: 2%</p>
-                            <p className='text-[clamp(16px,5vw,22px)] sm:text-sm'>Humidity: {weatherData[0]?.main.humidity}%</p>
-                            <p className='text-[clamp(16px,5vw,22px)] sm:text-sm'>Wind Speed: {weatherData[0]?.wind.speed}</p>
+                        <div className="today flex justify-between sm:items-center flex-col sm:flex-row gap-4">
+                            <div className="temperatureSide">
+                                <h1 className='text-[clamp(20px,20vw,60px)] sm:text-[60px]'>{Math.round(weatherData[0]?.main.temp)}<sup className=''>o</sup>C</h1>
+                                <p className='text-[clamp(16px,5vw,22px)] sm:text-sm'>Precepitation: 2%</p>
+                                <p className='text-[clamp(16px,5vw,22px)] sm:text-sm'>Humidity: {weatherData[0]?.main.humidity}%</p>
+                                <p className='text-[clamp(16px,5vw,22px)] sm:text-sm'>Wind Speed: {weatherData[0]?.wind.speed}</p>
+                            </div>
+
+                            <div className="location text-[clamp(16px,5vw,22px)] sm:text-sm">
+                                <p>{weatherData[0]?.name}, {weatherData[0]?.sys.country}</p>
+                                <p>{today}, {timeNow}</p>
+                            </div>
                         </div>
 
-                        <div className="location text-[clamp(16px,5vw,22px)] sm:text-sm">
-                            <p>{weatherData[0]?.name}, {weatherData[0]?.sys.country}</p>
-                            <p>{today}, {timeNow}</p>
+                        <div className="predictions hidden sm:flex jusstify-between gap-2">
+                            {
+                                otherDays.map((day, index: number) => {
+                                    return (
+                                        <div key={index} className="day py-4 px-2 flex-1 rounded-lg text-[10px] md:text-[14px] flex flex-col bg-black/50 backdrop-blur-lg gap-2 items-center">
+                                            <h1>{day.slice(0, 3)}</h1>
+                                            <div className="icon">
+                                                <Image className='w-10 h-10' src={`/images/weather/${weatherData[0]?.weather[0].icon}.png`} alt='icon' width={100} height={100} />
+
+                                            </div>
+                                            <p>{Math.round(weatherData[0]?.main.temp + Math.random() * (4 * 2) - 4)} <sup>o</sup> c</p>
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                     </div>
+                    :
+                    <div className='xl:w-1/2 flex items-center'>
 
-                    <div className="predictions hidden sm:flex jusstify-between gap-2">
-                        {
-                            [1, 2, 3, 4, 5, 6, 7].map((day, index: number) => {
-                                return (
-                                    <div key={index} className="day py-4 px-2 flex-1 rounded-lg text-[14px] flex flex-col bg-black/50 backdrop-blur-lg gap-2 items-center">
-                                        <h1>Tue</h1>
-                                        <div className="icon">ICON</div>
-                                        <p>30 <sup>o</sup> c</p>
-                                    </div>
-                                )
-                            })
-                        }
+                        <DropLoader />
                     </div>
-                </div>
-                :
-            <div className='xl:w-1/2 flex items-center'>
-
-                <DropLoader/>
-            </div>
             }
 
 
             {/* right side */}
             <div className="rightSide hidden text-white text-[1vmin] w-fit justify-end xl:w-1/2 sm:flex flex-wrap gap-2">
 
-                {   weatherData.length > 0 ?
+                {weatherData.length > 0 ?
                     weatherData?.slice(1, 5).map((weather: WeatherDataType, index: number) => {
                         return (
                             <div key={index} className={`city1 
@@ -174,7 +179,7 @@ const Weather = () => {
                         )
                     })
                     :
-                    <DropLoader/>
+                    <DropLoader />
                 }
 
             </div>
